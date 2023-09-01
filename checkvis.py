abab65127116f55589f07efeb398c8a5e24c5b94
mhrahmani <mohammad.rahmani@uantwerpen.be>
2023-09-01 15:02:49 +0200

    can define words now

--- checkvis.py
+++ checkvis.py
@@ -3,10 +3,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 import requests
 from bs4 import BeautifulSoup
 import os
+import sqlite3
 
 # Get the absolute path of the directory containing the script
 script_directory = os.path.dirname(os.path.abspath(__file__))
 token_path = os.path.join(script_directory, "bot-token.txt")
+db_path = os.path.join(script_directory, "data.db")
 
 try:
     with open(token_path, 'r') as file:
@@ -20,14 +22,7 @@ except Exception as e:
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Welcome to the Case Analysis Bot! Please send me a case number.")
 
-def analyze_case(update: Update, context: CallbackContext) -> None:
-    case_number = update.message.text
-
-    # Check if the entered message is a valid number
-    if not case_number.isdigit():
-        update.message.reply_text("Please enter a valid case number (a numeric value).")
-        return
-
+def analyze_case(case_number: int) -> str:
     url = f"https://infovisa.ibz.be/ResultNl.aspx?place=THR&visumnr={case_number}"
     
     response = requests.get(url)
@@ -56,14 +51,61 @@ def analyze_case(update: Update, context: CallbackContext) -> None:
             # No table found on the page!
             analysis_result = f"{case_number}: Error."
 
-    update.message.reply_text(analysis_result, parse_mode="Markdown")
+    return analysis_result
+
+
+def define(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    args = context.args
+
+    if len(args) != 2:
+        update.message.reply_text("Usage: /define <word> <case_number>")
+        return
+
+    word, case_number = args[0], args[1]
+
+    # Insert the association into the database (replace if it already exists)
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR REPLACE INTO user_commands (user_id, word, case_number) VALUES (?, ?, ?)", (user_id, word, case_number))
+    conn.commit()
+    conn.close()
+
+    update.message.reply_text(f"Defined: {word} => {case_number}")
+
+
+def get_association(update: Update, word: str) -> None:
+    user_id = update.message.from_user.id
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT case_number FROM user_commands WHERE user_id = ? AND word = ?", (user_id, word))
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        case_number = result[0]
+        update.message.reply_text(f"The case number for '{word}' is: {case_number}")
+    else:
+        update.message.reply_text(f"No association found for '{word}'")
+
+def check_message(update: Update, context: CallbackContext) -> None:
+    msg_text = update.message.text.strip()
+
+    # Check if the entered message is a valid number
+    if msg_text.isdigit():
+        analysis_result = analyze_case(msg_text)
+        update.message.reply_text(analysis_result, parse_mode="Markdown")
+    else:
+        get_association(update=update, word=msg_text)
 
 def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, analyze_case))
+    dp.add_handler(CommandHandler("define", define, pass_args=True))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, check_message))
 
     updater.start_polling()
     updater.idle()
